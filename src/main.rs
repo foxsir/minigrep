245360f03c6ef4467e2ef0bbe8e12bbc714584d9
foxsir <contact@umbst.com>
2024-01-22 21:40:06 +0800

    update minigrep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{env, process};
+
+use minigrep;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = minigrep::Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    minigrep::run(config).unwrap_or_else(|err| {
+        println!("Application error: {err}");
+        process::exit(1);
+    });
 }
